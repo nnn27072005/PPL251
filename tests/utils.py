5d000785a57d37b244374fc6520a21611257bb08
BlueSky-17 <thikhacquan.70@gmail.com>
2025-08-29 22:17:08 +0700

    Initial commit

---
+++ tests/utils.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import subprocess
+import tempfile
+import shutil
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), "build"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
+
+from antlr4 import *
+from build.OPLangLexer import OPLangLexer
+from build.OPLangParser import OPLangParser
+from src.utils.error_listener import NewErrorListener
+
+
+
+class Tokenizer:
+    def __init__(self, input_string):
+        self.input_stream = InputStream(input_string)
+        self.lexer = OPLangLexer(self.input_stream)
+
+    def get_tokens(self):
+        tokens = []
+        token = self.lexer.nextToken()
+        while token.type != Token.EOF:
+            tokens.append(token.text)
+            try:
+                token = self.lexer.nextToken()
+            except Exception as e:
+                tokens.append(str(e))
+                return tokens
+        return tokens + ["EOF"]
+
+    def get_tokens_as_string(self):
+        tokens = []
+        try:
+            while True:
+                token = self.lexer.nextToken()
+                if token.type == Token.EOF:
+                    tokens.append("EOF")
+                    break
+                tokens.append(token.text)
+        except Exception as e:
+            if tokens:  # If we already have some tokens, append error
+                tokens.append(str(e))
+            else:  # If no tokens yet, just return error
+                return str(e)
+        return ",".join(tokens)
+
+
+class Parser:
+    def __init__(self, input_string):
+        self.input_stream = InputStream(input_string)
+        self.lexer = OPLangLexer(self.input_stream)
+        self.token_stream = CommonTokenStream(self.lexer)
+        self.parser = OPLangParser(self.token_stream)
+        self.parser.removeErrorListeners()
+        self.parser.addErrorListener(NewErrorListener.INSTANCE)
+
+    def parse(self):
+        try:
+            self.parser.program()  # Assuming 'program' is the entry point of your grammar
+            return "success"
+        except Exception as e:
+            return str(e)
